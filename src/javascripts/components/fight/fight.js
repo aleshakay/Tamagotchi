57582e1bf3bf93478d3884bf8da7b3ceb5db2799
alesha reed <aleshak83@gmail.com>
2019-10-08 21:33:10 -0500

    added titles and buttons

--- src/javascripts/components/fight/fight.js
+++ src/javascripts/components/fight/fight.js
@@ -1,9 +1,33 @@
 import './fight.scss';
 import utilities from '../../helpers/utilities';
-import foods from '../data/eatHealth';
+
+const fights = [
+  {
+    id: 'push',
+    points: 10,
+  },
+  {
+    id: 'run',
+    points: 1,
+  },
+];
 
 const addColor = () => {
   document.getElementById('fight').classList.add('pull-left_bottom');
+};
+
+const createfightButton = () => {
+  let domString = '<div id="fightbtnSection">';
+  for (let i = 0; i < fights.length; i += 1) {
+    const fight = fights[i];
+    domString += `
+    <button id="${fight.id}" class="fight-button fight-button-${fight.id}">
+      <div>${fight.id}</div>
+    </button>
+  `;
+  }
+  domString += '</div>';
+  return domString;
 };
 
 const fightBuilder = () => {
@@ -14,14 +38,8 @@ const fightBuilder = () => {
     </div>
     <div class='row'></div>
   </div>`;
-  for (let i = 0; i < foods.length; i += 1) {
-    domString += `
-        <div id='unhealthybtn'>
-          <button class="cookies">${foods.id}</Button>
-        </div>
-      `;
-  }
   addColor();
+  domString += createfightButton(fights);
   utilities.printToDom('fight', domString);
 };
 
